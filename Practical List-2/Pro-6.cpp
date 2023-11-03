646ca98778f9ce92eab568d632d2a05760821772
Shyamalbharadava <143205298+Shyamalbharadava@users.noreply.github.com>
2023-11-03 16:09:53 +0530

    Create Pro-6.cpp

---
+++ Practical List-2/Pro-6.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	// function setw & endl
+	float m = 3.14567;
+	cout<<"I am Shyamal Bharadava "<<endl;
+	cout<<"En_no    220130318069 "<<endl;
+	cout<<"---------------------"<<endl;
+	//cout<<"The Value of A is ::"<<m;
+ 	cout<<"This is for function setprecision : "<<setprecision(3)<<m; // function setw 
+	return 0;
+}
+
