54390ad4e9bbb982742008a2e6dcdf32894b8a86
Shyamalbharadava <143205298+Shyamalbharadava@users.noreply.github.com>
2023-11-03 16:14:12 +0530

    Create Pro-12.cpp

---
+++ Practical List-2/Pro-12.cpp
@@ -0,0 +1,21 @@
+// CPP Program to demonstrate Default Arguments
+#include<iostream>
+using namespace std;
+int area(float r,float pi=3.14)
+{
+	float a;
+	a=pi*r*r;
+	cout<<"Area Of Circle is ::"<<a;
+}
+int main()
+{
+	int r;
+	cout<<"Enter The radius of The Circle ::";
+	cin>>r;
+	
+	area(r);
+
+	
+	return 0;
+	
+}
