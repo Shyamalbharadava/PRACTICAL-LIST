29c6a8268e213f70ffe7d2234eb101c6c2c36721
Shyamalbharadava <143205298+Shyamalbharadava@users.noreply.github.com>
2023-11-03 16:04:24 +0530

    Create Pro-1.cpp

---
+++ Practical List-2/Pro-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int m =1;
+int main()
+{
+    int m= 2;
+
+	cout<<"Shyamal Bharadava "<<endl;
+	cout<<"220130318069"<<endl;
+    cout << "The Value of Local Variables :: " <<m<<endl;
+    cout << "The Value of global scope resolution operator  :: " <<::m<<endl;
+  	return 0;
+}
