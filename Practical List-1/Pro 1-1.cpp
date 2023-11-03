d3ba000995a243a5b5985ebde63c18776c7b7029
Shyamalbharadava <143205298+Shyamalbharadava@users.noreply.github.com>
2023-11-03 12:37:42 +0530

    Create Pro 1-1.cpp

---
+++ Practical List-1/Pro 1-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+  cout<<"I am bheda dhruv "<<endl;
+	cout<<"En_no    220130318012"<<endl;
+    int num1, num2,sum,avg;
+    cout<<"Enter num 1 :: ";
+    cin>>num1;
+    cout<<"Enter num 2 :: ";
+    cin>>num2;
+    sum=num1+num2;
+    avg=num1+num2/2;
+    cout<<"Sum"<<" "<<sum<< endl;
+    cout<<"Avg"<<" "<<avg;
+    cout << "Name: Shyamal Bharadava." << endl;
+  cout << "Erollment number: 220130318069" << endl;
+}
