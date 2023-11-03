72183c3f8f954109e0f5fe8db032f78f7b7ca47b
Shyamalbharadava <143205298+Shyamalbharadava@users.noreply.github.com>
2023-11-03 16:09:12 +0530

    Create Pro-5.cpp

---
+++ Practical List-2/Pro-5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int a = 5;
+    int b = 10;
+    cout<<"I am Shyamal Bharadava "<<endl;
+	cout<<"En_no    220130318069 "<<endl;
+	cout<<"The value of a :: "<<setw(5)<<a<<endl;
+    cout<<"The value of b :: "<<setw(5)<<b<<endl;
+    
+    return 0;
+
+}
