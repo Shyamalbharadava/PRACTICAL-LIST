bb9ffc8bed32284db691a4ae61297ab302fada48
Shyamalbharadava <143205298+Shyamalbharadava@users.noreply.github.com>
2023-11-03 13:50:35 +0530

    Create Pro-9.cpp

---
+++ Practical List-1/Pro-9.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int n;
+	cout<<"I am Shyamal Bharadava"<<endl;
+	cout<<"En_no    220130318069 "<<endl;
+	cout<<"---------------------"<<endl;
+	cout<<"Enter the number : ";
+	cin>>n;
+	
+	if(n%2==0)
+	{
+		cout<<"This is Even number ";
+	}
+	else
+	{
+		cout<<"This is odd number ";
+	}
+	return 0;
+}
