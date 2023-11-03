1fc1e1b526de8000709b7316d3e77e9c893419d9
Shyamalbharadava <143205298+Shyamalbharadava@users.noreply.github.com>
2023-11-03 12:45:52 +0530

    Create Pro-3.cpp

---
+++ Practical List-1/Pro-3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "hello world";
+     cout<<"I am Shyamal Bharadava"<<endl;
+	cout<<"En_no    220130318069 "<<endl;
+	cout<<"---------------------"<<endl;
+  
+  return 0;
+}
