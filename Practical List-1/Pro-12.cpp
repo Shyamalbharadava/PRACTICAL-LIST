ded971be7fbc0219fd169c41e7845cc54d7e1035
Shyamalbharadava <143205298+Shyamalbharadava@users.noreply.github.com>
2023-11-03 14:04:21 +0530

    Create Pro-12.cpp

---
+++ Practical List-1/Pro-12.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int n,i;
+	
+	cout<<"I am Shyamal Bharadava "<<endl;
+	cout<<"En_no    220130318069 "<<endl;
+	cout<<"---------------------"<<endl;
+	cout<<"Enter the number : ";
+	cin>>n;
+	
+	for(i=1;i<10;i++)
+	{
+		cout<<n<<" * "<<i<<" = "<<n*i<<endl;
+	}
+	cout<<endl;
+}
