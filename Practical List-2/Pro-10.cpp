47990828a4721f05752d768136c0790233ff514c
Shyamalbharadava <143205298+Shyamalbharadava@users.noreply.github.com>
2023-11-03 16:12:41 +0530

    Create Pro-10.cpp

---
+++ Practical List-2/Pro-10.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int print(int &p,int &q);
+int main()
+{
+	int p,q;
+	cout<<"Shyamal Bharadava "<<endl;
+	cout<<"220130318069"<<endl; 
+	cout<<"Enter The Number P::";
+	cin>>p;
+	cout<<"Enter The Number Q::";
+	cin>>q;
+	
+	print(p,q);
+	return 0;
+}
+int print(int &p,int &q)
+{
+	cout<<"The Number's Multiplication is ::"<<p*q;
+}
